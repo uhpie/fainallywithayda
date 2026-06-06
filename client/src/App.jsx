b1a409eca5327d8ce00138166cf343ee0eaf44a7
Pie <pie@Pies-MacBook-Air.local>
2026-06-07 04:53:23 +0800

    fix lottie fullscreen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -164,6 +164,7 @@ export default function App() {
         background="transparent"
         speed="1"
         style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh', pointerEvents: 'none', zIndex: 9999 }}
+        preserveAspectRatio="xMidYMid slice"
         loop
         autoplay
       ></dotlottie-player>
